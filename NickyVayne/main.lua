16036f96b08873f31d36b48d6c7cf816f0254bab
JaceNithy <35814641+JaceNithy@users.noreply.github.com>
2018-06-26 17:51:54 -0300

    Add Logic

--- NickyVayne/main.lua
+++ NickyVayne/main.lua
@@ -19,8 +19,10 @@ local spellE = {
 }
 
 local spellR = {
-	range = 1000
+    range = 1000
 }
+
+local Invisible = false
 
 --[[local interruptspells = { "glacialstorm"; "caitlynaceinthehole";
  "ezrealtrueshotbarrage";
@@ -47,7 +49,7 @@ local spellR = {
 local menu = menu("Nicky [Vayne]", "Vayne: By Nicky")
 
 menu:menu("combo", "Combo")
-menu.combo:dropdown("Qmode", "AA Reset Mode: ", 1, {"Mouse[Pos]", "Pos AA", "Aggressive AA"})
+menu.combo:dropdown("Qmode", "AA Reset Mode: ", 1, {"GetSmartTumblePos", "GetKitingTumblePos"})
 menu.combo:slider("DistanceAA", "Distance AA", 300, 0, 300, 1)
 menu.combo:boolean("QC", "Use Q in Combo", true)
 menu.combo:boolean("EC", "Use Auto Condemn", true)
@@ -76,6 +78,22 @@ local function WStacks(args)
 	return spellW.stacks
 end
 
+local function GetDistanceSqr(Pos1, Pos2)
+	if Pos1 == nil or Pos2 == nil then
+		return math.huge
+    end
+	local Pos2 = Pos2 or vec3(player.pos)
+	local dx = Pos1.x - Pos2.x
+	local dz = (Pos1.z or Pos1.y) - (Pos2.z or Pos2.y)
+	return dx * dx + dz * dz
+end
+
+local function GetDistance(one, two)
+    if (not one or not two) then
+        return math.huge
+    end
+    return one:dist(two)
+end
 
 local function count_enemies_in_range(pos, range) -- ty Kornis Thank you for allowing your code
 	local enemies_in_range = {}
@@ -88,20 +106,102 @@ local function count_enemies_in_range(pos, range) -- ty Kornis Thank you for all
 	return enemies_in_range
 end
 
---[[local function updatebuff(args, unit)
-
+local function OnUpdateBuff(buff)
+    if buff.name == "vaynetumblefade" then
+        Invisible = true
+    end 
 end 
 
-local function removebuff(args, unit)
+local function OnRemoveBuff(buff)
+    if buff.name == "vaynetumblefade" then
+        Invisible = false
+    end 
+end 
 
-end ]]
+--[[local function StayInvisible()	
+	if Invisible == false or #count_enemies_in_range(player.pos, 350) == 0 then
+		orb.core.set_pause_attack(math.huge)
+		return
+	end
+
+    local inimigo = common.GetEnemyHeroes()
+    for i, v in ipairs(inimigo) do
+        local GetAttackRange = player.attackRange 
+    	if GetAttackRange < 400 and player.pos:dist(target.pos) == 350 then
+    		orb.core.set_pause_attack(0)
+    		return
+    	end
+    end
+end]]
+
+local function IsDangerousPosition(unit)
+--    if UnderTower(unit) then return true end
+    local inimigo = common.GetEnemyHeroes()
+    for i, v in ipairs(inimigo) do
+    	if not v.isDead and v.team == TEAM_ENEMY and unit:distSqr(v) < 300 * 300 then return true end
+    end
+   	return false
+end
+
+local function GetKitingTumblePos(target)
+    local possiblePos = player.pos:ext(game.mousePos, 300)   
+    if not IsDangerousPosition(possiblePos) and GetDistance(target.pos, possiblePos) > GetDistance(target.pos) then return possiblePos end
+end
+
+local function GetSmartTumblePos(target)
+	local possiblePos = player.pos:ext(game.mousePos, 300) or vec3(0,0,0)
+	local points= {
+	[1] = player.pos + vec3(300,0,0),
+	[2] = player.pos + vec3(277,0,114),
+	[3] = player.pos + vec3(212,0,212),
+	[4] = player.pos + vec3(114,0,277),
+	[5] = player.pos + vec3(0,0,300),
+	[6] = player.pos + vec3(-114,0,277),
+	[7] = player.pos + vec3(-212,0,212),
+	[8] = player.pos + vec3(-277,0,114),
+	[9] = player.pos + vec3(-300,0,0),
+	[10] = player.pos + vec3(-277,0,-114),
+	[11] = player.pos + vec3(-212,0,-212),
+	[12] = player.pos + vec3(-114,0,-277),
+	[13] = player.pos + vec3(0,0,-300),
+	[14] = player.pos + vec3(114,0,-277),
+	[15] = player.pos + vec3(212,0,-212),
+	[16] = player.pos + vec3(277,0,-114)}
+	
+	for i=1,#points do		
+		if IsDangerousPosition(points[i]) == false and points[i]:distSqr(target) < 500 * 500 then
+			if (navmesh.isWall(target.pos:ext(player.pos, -450))) then 
+				return points[i]
+			end 
+		end
+	end
+
+	if IsDangerousPosition(possiblePos) == false then
+		return possiblePos
+	end
+
+	
+	for i=1,#points do
+		if IsDangerousPosition(points[i]) == false and points[i]:distSqr(target) < 500 * 500  then 
+			return points[i]
+		end
+	end	
+	return nil
+end
+
+local function GetAggressiveTumblePos(target)
+    --local mousePos = vec3(game.mousePos)
+    --local targetPos = vec3(target)
+    if target.pos:dist(game.mousePos) < target.pos:dist(player.pos) then return game.mousePos end
+end
+
 
 local function OnTick()
     local inimigo = common.GetEnemyHeroes()
     for i, target in ipairs(inimigo) do
         
     --if orb.combat.target then
-        if target and target.isVisible and menu.combo.EC:get() and player.pos:dist(target.pos) < 1000 then
+        if target and target.isVisible and menu.combo.EC:get() and player.pos:dist(target.pos) <= 700 then
             local enemiesCount = #count_enemies_in_range(player.pos, 1200)
             if 	enemiesCount > 1 and enemiesCount <= 3 then
                 for i= 15, 450, 75 do
@@ -136,9 +236,9 @@ local function OnTick()
 	if not target then return end
     if menu.combo.Qmode:get() == 2 and orb.combat.target and menu.chave.CK:get() then
     	local range = menu.combo.DistanceAA:get() + player.attackRange + (player.boundingRadius + target.boundingRadius)
-		if target.pos:dist(player.pos) <= range then
-			player:castSpell("pos", 0, game.mousePos)
-		end
+        if target.pos:dist(player.pos) <= range then
+            player:castSpell("pos", 0, game.mousePos)
+        end 
     end  
     local target = obtermeta();
     if not target then return end    
@@ -154,6 +254,8 @@ local function OnTick()
             player:castSpell("obj", 2, target)
         end 
     end 
+   -- StayInvisible()
+   --AggressiveTrumble()
 end 
 
 local function IsWRange()
@@ -186,18 +288,12 @@ local function OnPreAttack()
 	local QRange = player.attackRange + target.boundingRadius
     if menu.combo.Qmode:get() == 1 and orb.combat.target then
         if player.pos:dist(target.pos) < QRange then
-            player:castSpell("pos", 0, game.mousePos)
-            orb.combat.set_invoke_after_attack(false)
+            local Qpos = GetSmartTumblePos(target)
+            --if Qpos ~= nil then
+                player:castSpell("pos", 0, vec3(Qpos.x, Qpos.y, Qpos.z))
+                orb.combat.set_invoke_after_attack(false)
+           --end 
         end
-    end 
-    if menu.combo.Qmode:get() == 3 and orb.combat.target then
-        local QIsRange = player.attackRange + (player.boundingRadius + target.boundingRadius)
-        local QisPred = { delay = 0.5, width = math.huge, speed = 1000, boundingRadiusMod = 1, collision = { hero = false, minion = false, }}
-        local PredLinear = pred.linear.get_prediction(QisPred, target)
-        if player.pos:dist(target.pos) < QIsRange then
-            player:castSpell("pos", 0, vec3(PredLinear.endPos.x, game.mousePos.y, PredLinear.endPos.y))
-            orb.combat.set_invoke_after_attack(false)
-        end 
     end 
 end 
     
@@ -227,5 +323,5 @@ cb.add(cb.draw, OnDraw)
 orb.combat.register_f_after_attack(OnPreAttack)
 orb.combat.register_f_pre_tick(OnTick)
 orb.combat.register_f_out_of_range(IsWRange)
---cb.add(cb.updatebuff, updatebuff)
---cb.add(cb.removebuff, removebuff)
+cb.add(cb.updatebuff, OnUpdateBuff)
+cb.add(cb.removebuff, OnRemoveBuff)
