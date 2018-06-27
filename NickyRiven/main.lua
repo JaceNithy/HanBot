5f6f1daca08f2ffe19a5890e4b9e74bc628d4074
JaceNithy <35814641+JaceNithy@users.noreply.github.com>
2018-06-27 19:01:59 -0300

    att

--- NickyRiven/main.lua
+++ NickyRiven/main.lua
@@ -12,6 +12,17 @@ local dmglib = avada_lib.damageLib
 local NickyRiven = { }
 local Spells = { }
 
+local function count_enemies_in_range(pos, range) -- ty Kornis Thank you for allowing your code
+	local enemies_in_range = {}
+	for i = 0, objManager.enemies_n - 1 do
+		local enemy = objManager.enemies[i]
+		if pos:dist(enemy.pos) < range and common.IsValidTarget(enemy) then
+			enemies_in_range[#enemies_in_range + 1] = enemy
+		end
+	end
+	return enemies_in_range
+end
+
 --Variable
 
 NickyRiven.FlashRange = 425 + 70 + 225 -- == self.FlashRange
@@ -64,6 +75,7 @@ local SpellsQr = { Range = 260, QCout = 1}
 local SpellsWr = { Range = 225 }
 local SpellsEr = { Range = 325 }
 local SpellsRr = { Range = 1100,  Width = 160, Delay = 0.25, Speed = 1600}
+local RPsred = { delay = 0.25, width = 160, speed = 1600, boundingRadiusMod = 1, collision = { hero = false, minion = false,}}
 
 local function LoadingRiven()
     NickyRiven:OnLoad()
@@ -91,6 +103,7 @@ function NickyRiven:OnLoad()
     cb.add(cb.spell, function(spell) self:OnProcessSpell(spell) end)
     cb.add(cb.updatebuff, function(buff) self:OnUpdateBuff(buff) end)
     cb.add(cb.removebuff, function(buff) self:OnRemoveBuff(buff) end)
+    orb.combat.register_f_after_attack(function() self:OnPreAttack() end)
 end 
 
 
@@ -121,6 +134,13 @@ function NickyRiven:OnTick()
     end 
     self:LeituraSpell()
    -- player:castSpell("pos", Flash4, game.mousePos)
+ --  self:CanCastTimat() 
+    if self.mymenu.ri.UT:get() then
+        local inimigo = common.GetEnemyHeroes()
+        for i, target in ipairs(inimigo) do
+            self:CanCastTimat(target)
+        end        
+    end 
 end 
 
 function NickyRiven:BurtsMode()
@@ -130,14 +150,17 @@ function NickyRiven:BurtsMode()
     end 
     local inimigo = common.GetEnemyHeroes()
     for i, target in ipairs(inimigo) do
-        if target and player:dist(target) <= range then
-            if orb.core.can_attack() then
-                player:attack(target)
-            end 
+        if target and player.pos:dist(target.pos) <= range then
             if player:spellSlot(2).state == 0 then
-                player:castSpell("pos", 0, target)
+                player:castSpell("pos", 2, target.pos)
             end 
-            if player:spellSlot(Flash4).state == 0  and player:spellSlot(1).state == 0 then
+            if player:spellSlot(3).state == 0 then
+                self:CastR(target)
+            end 
+            if player:spellSlot(1).state == 0 and player.pos:dist(target.pos) <= 225 + target.boundingRadius then
+                player:castSpell("self", 1)
+            end 
+            if player:spellSlot(Flash4).state == 0 and player:spellSlot(1).state == 0 then
                 self:FlashW(target)
             end 
         end 
@@ -149,13 +172,27 @@ function NickyRiven:FlashW(target)
     local flashPos = target.pos + (target.pos - player.pos):norm() * -180
   
     if self.RAttackRangeBoost or player:spellSlot(3).state == 0 then
-        player:castSpell('obj', 1, player)
-        common.DelayAction(function() player:castSpell("pos", Flash4, flashPos) end, 0.2)
+   --     player:castSpell("self", 1)
+        common.DelayAction(function() player:castSpell("pos", Flash4, vec3(flashPos)) end, 0.2)
     else
-        player:castSpell("pos", Flash4, flashPos)
-        player:castSpell('obj', 1, player)
+        player:castSpell("pos", Flash4, vec3(flashPos))
+        --player:castSpell("self", 1)
     end
 end
+
+function NickyRiven:CastR(target)
+    if self.RWindslashReady then
+        local RPred = pred.linear.get_prediction(RPsred, target)
+        if RPred and RPred.startPos:dist(RPred.endPos) < SpellsRr.Range then
+            self:CanCastTimat(target)
+            common.DelayAction(function() player:castSpell("pos", 3, vec3(RPred.endPos.x, RPred.endPos.y, RPred.endPos.y)) end, 0.25)
+        end
+        player:castSpell("pos", 3, vec3(RPred.endPos.x, RPred.endPos.y, RPred.endPos.y))
+        orb.core.reset() 
+    else 
+        player:castSpell("self", 3)
+    end
+end 
 
 function NickyRiven:LeituraSpell()
     if player:spellSlot(4).name == "SummonerFlash" then
@@ -163,6 +200,72 @@ function NickyRiven:LeituraSpell()
       elseif player:spellSlot(5).name == "SummonerFlash" then
         Flash4 = 5
     end
+end 
+
+function NickyRiven:Reset()
+    player:move(game.mousePos)
+    orb.core.set_pause_move(0)
+    orb.core.set_pause_attack(0)
+    orb.core.reset() 
+end
+
+function NickyRiven:CastQ(target)
+    local delay = math.min(60/1000, network.latency/1000)
+    local coreDelay =  0.21 - (0.008 * player.levelRef)
+    delay = delay + coreDelay
+  
+    if SpellsQr.QCout == 3 then
+      delay = delay + 0.03
+    end
+    if self:CanCastTimat(target) and SpellsQr.QCout == 3 then
+        self:CanCastTimat(target) 
+        common.DelayAction(function() player:castSpell('obj', 0, target) end, 0.23)
+    else
+        player:castSpell('obj', 0, target)
+    end
+    orb.core.set_pause_move(math.huge)
+    orb.core.set_pause_attack(math.huge)
+    common.DelayAction(function() self:Reset() end, delay)
+end 
+
+function NickyRiven:CanCastTimat(target)
+   -- local inimigo = common.GetEnemyHeroes()
+   -- for i, target in ipairs(inimigo) do
+        if (target.pos:dist(player) <= 275) then
+            for i = 6, 11 do
+                local item = player:spellSlot(i).name
+                if item and (item == "ItemTiamatCleave") then
+                    player:castSpell("obj", i, target)
+                end
+                if item and (item == "ItemTitanicHydraCleave") then
+                    player:castSpell("obj", i, target)
+                end
+            end 
+        end 
+   -- end 
+end 
+
+function NickyRiven:OnPreAttack()
+    local inimigo = common.GetEnemyHeroes()
+    for i, target in ipairs(inimigo) do
+        if target and target.isVisible and common.IsValidTarget(target) then
+            if self.mymenu.ri.CK:get() then
+                if player:spellSlot(3).state == 0 then
+                    self:CastR(target)
+                end
+                if self.RWindslashReady then
+                    common.DelayAction(function() self:CastQ(target) end, 0.25)
+                end 
+            elseif player:spellSlot(0).state == 0  then
+                self:CastQ(target)
+            end 
+            if orb.combat.target then
+                if player:spellSlot(3).state == 0 then
+
+                end 
+            end 
+        end 
+    end 
 end 
 
 function NickyRiven:OnDraw()
@@ -197,6 +300,7 @@ function NickyRiven:OnProcessSpell(spell)
     end
 end 
 
+
 function NickyRiven:OnUpdateBuff(buff)
     if buff.name == "RivenFeint" then
         self.ELastCastTick = os.clock()
@@ -206,6 +310,34 @@ end
 function NickyRiven:OnRemoveBuff(buff)
 --Rs
 end 
+
+function NickyRiven:ComboDamage(target)
+
+end 
+
+function NickyRiven:RealDamage(target, damage)
+    if target.buff["KindredRNoDeathBuff"] or target.buff["JudicatorIntervention"] or target.buff["FioraW"] or target.buff["ShroudofDarkness"]  or target.buff["SivirShield"] then
+        return 0  
+    end
+    local pbuff = target.buff["UndyingRage"]
+    if target.buff["UndyingRage"] and pbuff.endTime > os.clock() + 0.3  then
+        return 0
+    end
+    local pbuff2 = target.buff["ChronoShift"]
+    if target.buff["ChronoShift"] and pbuff2.endTime > os.clock() + 0.3 then
+        return 0
+    end
+    if myHero.buff["SummonerExhaust"] then
+        damage = damage * 0.6;
+    end
+    if target.buff["BlitzcrankManaBarrierCD"] and target.buff["ManaBarrier"] then
+        damage = damage - target.MP / 2
+    end
+    if target.buff["GarenW"] then
+        damage = damage * 0.6;
+    end
+    return damage
+end
 
 --Loading
 LoadingRiven()
