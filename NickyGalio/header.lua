4a879a565005ba806d34462309f3145f67281e9f
JaceNithy <35814641+JaceNithy@users.noreply.github.com>
2018-06-29 12:28:33 -0300

    Devs

---
+++ NickyGalio/header.lua
@@ -0,0 +1,7 @@
+return {
+    id = 'NickyGalio',
+    name = 'NickyGalio',
+    load = function()
+      return player.charName == 'Galio'
+    end
+}
---
+++ NickyGalio/main.lua
@@ -0,0 +1,14 @@
+local orb = module.internal("orb");
+local evade = module.internal("evade");
+local pred = module.internal("pred");
+local ts = module.internal('TS');
+
+local avada_lib = module.lib('avada_lib')
+--Avada Lib
+local common = avada_lib.common
+local dmglib = avada_lib.damageLib
+
+
+local NickyGalio = { }
+local Spells = { }
+
