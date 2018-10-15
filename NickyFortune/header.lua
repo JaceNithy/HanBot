9a778f266189b76462e0ccdc37f1db474af42ce1
JaceNithy <35814641+JaceNithy@users.noreply.github.com>
2018-10-15 17:53:56 -0300

    Delete header.lua

--- NickyFortune/header.lua
+++
@@ -1,9 +0,0 @@
-return {
-    id = "NickyMissFortune".. player.charName,
-    name = "[Nicky]MissFortune",
-    riot = true,
-    type = "Champion",
-    load = function()
-      return player.charName == "MissFortune"
-    end
-}
